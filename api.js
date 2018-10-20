2770c061b6ec7e07370dce0693c4357fdc4358b4
eloimuns <eloimuns@gmail.com>
2018-10-20 14:25:49 +0200

    push changes

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
 'use strict';
-
+var token = "9d7aceca-d575-400d-b647-c6b500ed6ce9";
 var bodyParser = require('body-parser');
 var path = require("path");
 var express = require('express');
 var app = express();
 var keys = require("./key")
+var request = require('request');
 
 var Client = require('node-rest-client').Client; 
 var client = new Client();
@@ -32,15 +33,27 @@ var server = app.listen(port, function () {
     console.log('Listening on port ' + server.address().port);
 });
 
+var args = {
+    headers : { "Authorization" : "Basic N2I5ZjMwMGNiZjUxNGNhOGJjOGIxMDI3NTk5OWE2ZGQ6eHFKVzBKbEJvcjJrUlVBbG5Pd050Z1U2RlNlT3dGUmtkQXJjVEpyUWI0UXg4ZkJCSTg=, Bearer " + token}
+};
+
+
+var getCvs = function(callback){
+    client.get("https://api.infojobs.net/api/2/curriculum", args, function (data, response) {
+        callback(data);
+    });
+}
+
+getCvs(function(result){
+    console.log(result)
+});
+/*
 app.get('/test', function(req, res){
-    var args = {
-        headers : { "Authorization" : "Basic N2I5ZjMwMGNiZjUxNGNhOGJjOGIxMDI3NTk5OWE2ZGQ6eHFKVzBKbEJvcjJrUlVBbG5Pd050Z1U2RlNlT3dGUmtkQXJjVEpyUWI0UXg4ZkJCSTg=,Bearer 9d7aceca-d575-400d-b647-c6b500ed6ce9"}
-    };
 
     client.get("https://api.infojobs.net/api/1/curriculum/419133b7-8d17-480d-91a8-f0b9cb6a609b/cvtext", args, function (data, response) {
         // parsed response body as js object
-        console.log(data);
+        callback()
     });
 
     res.send('...');
-});
+});*/
